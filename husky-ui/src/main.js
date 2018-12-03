b88e859d691df02b9a48cc63fc5ce936f9477558
caojiantao <caojiantao30@gmail.com>
2018-12-04 00:21:36 +0800

    vue3.0风格

--- husky-ui/src/main.js
+++ husky-ui/src/main.js
@@ -1,15 +1,14 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.system.conf with an alias.
 import Vue from 'vue'
-import App from './App'
+import './plugins/axios'
+import App from './App.vue'
+import store from './store'
 import router from './router'
+import './plugins/element.js'
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
 new Vue({
-  el: '#app',
+  store,
   router,
-  components: { App },
-  template: '<App/>'
-})
+  render: h => h(App)
+}).$mount('#app')
---
+++ husky-ui/src/views/Login.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="login">
+    <el-form ref="loginForm" :model="loginForm.model" :label-width="loginForm.labelWidth">
+        <el-form-item label="账号">
+          <el-input v-model="loginForm.model.username"></el-input>
+        </el-form-item>
+          <el-form-item label="密码">
+          <el-input type="password" v-model="loginForm.model.password"></el-input>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary" @click="submitForm">提交</el-button>
+          <el-button @click="resetForm">重置</el-button>
+        </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'login',
+  components: {
+  },
+  data: () => {
+    return {
+      loginForm: {
+        model: {},
+        labelWidth: '80px'
+      }
+    }
+  },
+  methods: {
+    submitForm: function() {
+      console.log(this.loginForm.model)
+    },
+    resetForm: function() {
+
+    }
+  }
+}
+</script>
